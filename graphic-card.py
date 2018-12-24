e84978da87e3321e7037b233c432641c14904e87
Quentin Laplanche <quentin.laplanche@outlook.fr>
2018-12-24 16:20:01 +0100

    Script is now only runnable by root user

--- graphic-card.py
+++ graphic-card.py
@@ -2,15 +2,20 @@
 
 # Graphic Card Status and Switching automation script
 
+import os
+import sys
+
 PWR_INDICATOR = "Pwr"
 SPLITTING_CHAR = ":"
 VGASWITCHEROO_FILEPATH = "/sys/kernel/debug/vgaswitcheroo/switch"
+
 
 class GraphicCardState():
     ON = "Pwr"
     OFF = "OFF"
     DYN_OFF = "DYN_OFF"
     UNKNOWN = "UNKNOWN"
+
 
 class GraphicCard():
     def __init__(self, name):
@@ -20,11 +25,8 @@ class GraphicCard():
 
     def __str__(self):
         partial_state = "{0} card is {1} ".format(self.name, self.state)
-        if self.in_use:
-            partial_state += "and in use."
-        else:
-            partial_state += "and not in use."
-        return partial_state
+        return partial_state + "and in use." if self.in_use else partial_state + "and not in use."
+
 
 class GraphicCardSwitchingTool():
     def __init__(self, file_status_path):
@@ -37,33 +39,26 @@ class GraphicCardSwitchingTool():
             for line in file.readlines():
                 data = line.split(SPLITTING_CHAR)
                 tmp_graphcard = GraphicCard(data[1])
-                if data[3] == PWR_INDICATOR:
-                    tmp_graphcard.state = GraphicCardState.ON
-                    #print("{0} is powered on".format(data[1]))
-                else:
-                    tmp_graphcard.state = GraphicCardState.OFF
+                tmp_graphcard.state = GraphicCardState.ON if data[
+                    3] == PWR_INDICATOR else GraphicCardState.OFF
 
-                if data[2] == "+":
-                    tmp_graphcard.in_use = True
-                else:
-                    tmp_graphcard.in_use = False
-                    
+                tmp_graphcard.in_use = True if data[2] == "+" else False
+
                 self.graphic_cards.append(tmp_graphcard)
-    
+
     def get_state(self):
         for graph_card in self.graphic_cards:
             print(graph_card)
 
+def check_super_user():
+    if not os.geteuid() == 0:
+        sys.exit("This Script must be used by root.")
+
 
 def main():
-    print("WARNING ! Script Must be launch as root user !\n\n")
+    check_super_user()
     tool = GraphicCardSwitchingTool(VGASWITCHEROO_FILEPATH)
     tool.get_state()
-
-    
-
-
-
 
 if __name__ == "__main__":
     main()
